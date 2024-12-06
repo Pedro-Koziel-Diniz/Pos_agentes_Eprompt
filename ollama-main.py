b1305b8195f3138b00913cc2b0ae3c4f4e30d709
Marcos Vinicius <63621170+marcosvinism@users.noreply.github.com>
2024-12-06 19:19:23 -0300

    Update ollama-main.py

--- ollama-main.py
+++ ollama-main.py
@@ -44,10 +44,7 @@ def resposta_llama_json(especificacoes, response_type=None, model="ollama/llama3
     return response_content
 
 especificacoes = input("Insira as informações necessárias para te ajudar a elaborar uma dieta, como seu sexo, idade, altura, peso, estilo de vida e objetivos: ")
-
 validator = resposta_llama_json(especificacoes)
-#Meu nome é Marcos, tenho 22 anos, 1.85 de altura, 80 quilos, pratico esportes regurlarmente, e tenho o objetivo de perda de peso.
-
 
 # Diretório onde os arquivos serão salvos
 output_directory = "outputs"
